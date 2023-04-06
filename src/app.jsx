472819289e36a5afc91e8ba1dee403ab818aa695
Vaibhavk0192 <vkapoor2_be21@thapar.edu>
2023-04-07 03:35:58 +0530

    faltu text remove

--- src/app.jsx
+++ src/app.jsx
@@ -3,14 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./container/Home/Home";
 import AboutUs from "./container/AboutUs/about_us";
 import Navbar from "./components/Navbar/Navbar";
-<<<<<<< HEAD
+
 import "./app.css";
 import Subscribe from "./container/Subscribe/Subscribe";
 import Login from "./container/Login/Login";
 import Signup from "./container/SignUp/Signup";
-=======
-import "./App.css";
->>>>>>> 84962efaa37670d215614f87c86e0d06ff373c59
+
 
 const App = () => {
   return (
