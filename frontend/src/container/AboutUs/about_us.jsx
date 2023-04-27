d2fd8bdab987ede4affe2e84a1eb7e8b6f187db5
Vaibhavk0192 <vkapoor2_be21@thapar.edu>
2023-04-28 00:30:35 +0530

    about us changed

--- frontend/src/container/AboutUs/about_us.jsx
+++ frontend/src/container/AboutUs/about_us.jsx
@@ -1,34 +1,40 @@
 import React from "react";
 import "./about_us.css";
 import "../../styles/app.css";
+import Vaibhav from "../../assets/vaibhav.jpeg";
+import ansh from "../../assets/ansh.jpeg";
+import pulkit from "../../assets/pulkit.jpeg";
 
 const about_us = () => {
   return (
     <div>
-      <div className="app__box">
+      <div className="app__box app">
         <div className="design_box1"></div>
         <div className="design_box2"></div>
-      
-          <h1 className="app__about-headtext">ABOUT US</h1>
-          <div className="app__about-content">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel unde,
-            accusamus nulla reprehenderit tempore fugit neque enim? Voluptate
-            doloremque ex sapiente ab accusantium tempore natus magnam
-            voluptatem, dolore tempora harum consequuntur quos eveniet aliquam.
-            Sequi, culpa quaerat officia architecto quasi voluptates cum sed
-            necessitatibus quo consectetur sit nesciunt, autem excepturi
-            perspiciatis. Culpa optio tempora iure veniam quo nulla nam delectus
-            minus voluptatum possimus est, praesentium, ducimus ipsum ratione
-            vero iusto ullam placeat maiores corporis accusantium hic,
-            laudantium eligendi? Quibusdam, nam beatae suscipit neque minus
-            ipsam iusto doloremque repellendus ducimus ipsa! Blanditiis sequi
-            nulla itaque reprehenderit! Dolore in rem expedita minima?
-          </div>
+        <h1 className="app__about-headtext">ABOUT US</h1>
+        <div className="app__about-content">
+          <div className="app__about-profile-box">
+            <div className="app__about-image">
+              <img src={Vaibhav} alt="" />
 
+              <div className="image-heading">Vaibhav Kapoor</div>
+              <div className="image-text">Designer & Frontend Developer</div>
+            </div>
+            <div className="app__about-image">
+              <img src={ansh} alt="" />
+              <div className="image-heading">Ansh Midha</div>
+              <div className="image-text">Backend Developer</div>
+            </div>
+            <div className="app__about-image">
+              <img src={pulkit} alt="" />
+              <div className="image-heading">Pulkit Saraf</div>
+              <div className="image-text">Frontend Developer</div>
+            </div>
+          </div>
+        </div>
         <div className="design_box3"></div>
         <div className="design_box4"></div>
-        </div>
-
+      </div>
     </div>
   );
 };
